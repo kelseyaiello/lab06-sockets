2894a29354a1f9b6119210ffff23bf2f7ffefd2e
Kelsey Aiello <kelseymaiello@gmail.com>
2017-02-21 10:58:04 -0800

    completed lab

--- build/client.js
+++ build/client.js
@@ -2,10 +2,34 @@
 var ip = require("ip");
 var net = require('net'), readline = require('readline'), client = new net.Socket(), io = readline.createInterface(process.stdin, process.stdout);
 client.on('data', function (data) {
+    console.log("Received: " + data + '/n');
 });
 client.on('close', function () {
+    console.log('server disconnected');
+    console.log('closing client');
+    process.exit(0);
 });
 var HOST = ip.address();
 var PORT = 3000;
 client.connect(PORT, HOST, function () {
+    console.log('Connencted to: ' + HOST + ":" + PORT);
+    io.setPrompt('> ');
+    io.prompt();
+    io.on('line', function (line) {
+        switch (line.trim()) {
+            case 'exit':
+                client.end();
+                console.log('client disconnected');
+                process.exit(0);
+                break;
+            default:
+                client.write(line);
+                break;
+        }
+        io.prompt();
+    }).on('close', function () {
+        client.end();
+        console.log('client disconnected');
+        process.exit(0);
+    });
 });
--- src/client.ts
+++ src/client.ts
@@ -9,11 +9,13 @@ const net = require('net'),
     
 
 client.on('data', function(data) { //when we get data
-    
+    console.log("Received: " + data + '/n'); //output
 });
 
 client.on('close', function() { //when connection closed
-    
+    console.log('server disconnected');
+    console.log('closing client');
+    process.exit(0);
 });
 
 
@@ -22,6 +24,30 @@ var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+    console.log('Connencted to: ' + HOST + ":" + PORT);
+
+        // prompt the user
+    io.setPrompt('> ');
+    io.prompt();
+
+    io.on('line', function(line){
+        switch(line.trim()) {
+        case 'exit':
+            client.end();
+            console.log('client disconnected');
+            process.exit(0);
+            break;
+        default:
+            client.write(line);
+            break;
+        }
+
+        io.prompt();
+    }).on('close', function() {
+        client.end();
+        console.log('client disconnected');
+        process.exit(0);
+    });
 
 });
 
